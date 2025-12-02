517f61dc2859df433a231854c8b62432a37d762d
Cyrino <52038657+joaopedrocyrino@users.noreply.github.com>
2025-12-02 10:52:56 -0300

    release config

--- release.config.js
+++ release.config.js
@@ -1,15 +1,27 @@
 module.exports = {
-    branches: ["master"],
-    plugins: [
+    "branches": ["main"],
+    "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        ["@semantic-release/npm", {
-            "npmPublish": true
-        }],
-        ["@semantic-release/git", {
-            "assets": ["package.json", "CHANGELOG.md"]
-        }],
-        "@semantic-release/github"
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "CHANGELOG.md"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["CHANGELOG.md", "package.json"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]"
+            }
+        ],
+        [
+            "@semantic-release/github",
+            {
+                "assets": "dist/**/*"
+            }
+        ]
     ]
-};
+}
+
